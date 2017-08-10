f70c5af959f090bbfdee73ce15ab281b3ab970b9
alex0777 <clash0080clans@gmail.com>
2017-08-11 00:52:02 +0430

    nimakm.ir

---
+++ plugins/online.lua
@@ -0,0 +1,11 @@
+﻿local function run(msg, matches)
+tg.sendMessage(msg.chat_id, 0, 0,  "\nبهترین نیستیم ولی بهترین ها ما را انتخاب کردند \nربات های ریان بیشتر در چنل ما \nکانال :\n @Cafegrm", 0)
+end
+return {
+  patterns = {
+	"^[Nn]erkh$",
+  },
+  run = run
+}
+
+
